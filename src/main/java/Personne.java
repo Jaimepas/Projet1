6d3e8f4a8e4f79ce53b80364ba5147d628e5f49d
jadorefinalement <emericdb0@gmail.com>
2026-04-10 16:54:15 +0200

    test
    # Conflicts:
    #	src/main/java/Passager.java
    #	src/main/java/Personne.java

--- src/main/java/Personne.java
+++ src/main/java/Personne.java
@@ -1,2 +1,18 @@
-public class Personne {
+public abstract class Personne {
+    protected String identifiant;
+    protected String nom;
+    protected String adresse;
+    protected String contact;
+
+    public Personne(String identifiant, String nom, String adresse, String contact) {
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+
+    // Méthode demandée pour afficher les infos [cite: 92]
+    public void obtenirInfos() {
+        System.out.println("ID: " + identifiant + " | Nom: " + nom);
+    }
 }
